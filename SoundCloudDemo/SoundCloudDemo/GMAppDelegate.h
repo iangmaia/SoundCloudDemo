abfe1522c8f8337df66b2cbe3fe6407a0d5d5e9e
Ian Guedes Maia <iangmaia@gmail.com>
2012-08-14 23:41:27 +0200

    GitHub for Mac: Throw-away commit.

---
+++ SoundCloudDemo/SoundCloudDemo/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  SoundCloudDemo
+//
+//  Created by Ian Guedes Maia on 8/14/12.
+//  Copyright (c) 2012 Ian Guedes Maia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoundCloudDemo/SoundCloudDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoundCloudDemo
+//
+//  Created by Ian Guedes Maia on 8/14/12.
+//  Copyright (c) 2012 Ian Guedes Maia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GMAppDelegate class]));
+	}
+}
